ce8f8a8d510cde94b256e7ad26cbeb6372b0f159
okkursevi1 <okkursevi1@UC5PC10>
2019-11-12 20:03:12 +0100

    value object and enum

--- FirstHibernateProject/src/controller/HibernateDAO.java
+++ FirstHibernateProject/src/controller/HibernateDAO.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import model.Car;
+import model.User;
 
 public class HibernateDAO {
 	
@@ -20,6 +21,24 @@ public class HibernateDAO {
 			
 			try {		
 				sesija.save(car);
+				sesija.getTransaction().commit();
+				return true;
+			} catch (Exception e) {
+				sesija.getTransaction().rollback();
+				return false;
+			}finally {
+				sesija.close();
+			}	
+	}
+	
+	
+	public boolean snimiUseraUbazu(User user) {
+		
+		Session sesija = factory.openSession();
+			sesija.beginTransaction();
+			
+			try {		
+				sesija.save(user);
 				sesija.getTransaction().commit();
 				return true;
 			} catch (Exception e) {
